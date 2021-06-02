f7c2cc679c7896f96193e68f470103a4acdc42de
shivamsingh124 <shivamsingh12042018@gmail.com>
2021-06-02 15:08:38 +0530

    hakathon pepcoding

--- module1/hackathonpepcoding/hackathon.js
+++ module1/hackathonpepcoding/hackathon.js
@@ -81,8 +81,7 @@ async function search(uid) {
             await tab.type('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr', "These people are willing to donate covid equipments : "+message);         
             await tab.waitForTimeout(10000)     //messgage
             await tab.click('path[d="M21.13 11.358L3.614 2.108c-.29-.152-.64-.102-.873.126-.23.226-.293.577-.15.868l4.362 8.92-4.362 8.92c-.143.292-.08.643.15.868.145.14.333.212.523.212.12 0 .24-.028.35-.087l17.517-9.25c.245-.13.4-.386.4-.664s-.155-.532-.4-.662zM4.948 4.51l12.804 6.762H8.255l-3.307-6.76zm3.307 8.26h9.498L4.948 19.535l3.307-6.763z"]', { visible: true })      // submit
-            // await tab.waitForTimeout(7000)
-            // await waitandclick('a[href="/explore"] .css-1dbjc4n.r-1awozwy.r-sdzlij.r-18u37iz.r-1777fci.r-dnmrzs.r-xyw6el.r-o7ynqc.r-6416eg',{visible:true}) 
+            await waitandclick('a[href="/explore"] .css-1dbjc4n.r-1awozwy.r-sdzlij.r-18u37iz.r-1777fci.r-dnmrzs.r-xyw6el.r-o7ynqc.r-6416eg',{visible:true}) 
             await scb();
         }
         catch {
